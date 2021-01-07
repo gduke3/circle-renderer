4434e4df1ce75c0055d83f1820f7a8042cd37f18
Nikita Osin <phoenix.ofdarkness@mail.ru>
2021-01-08 04:38:16 +0500

    fix Circle background in Circle is highlighted

--- src/components/common/Circle/styled.ts
+++ src/components/common/Circle/styled.ts
@@ -2,11 +2,12 @@ import styled from "styled-components";
 
 const Circle = styled.div<CircleProps>`
 	position: absolute;
+	border-radius: 50%;
 	width: ${(props) => props.$diameter}px;
 	height: ${(props) => props.$diameter}px;
 	top: ${(props) => props.$y}px;
 	left: ${(props) => props.$x}px;
-	background: ${(props) => (props.$isHighlighted ? "yellow" : "black")};
+	background: ${(props) => (props.$isHighlighted ? "green" : "black")};
 `;
 
 export { Circle };
